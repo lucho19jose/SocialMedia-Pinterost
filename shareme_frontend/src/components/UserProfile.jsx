abe79ac50257b3669449f8aa540482b19b216cda
lucho19jose <barbozagonzalesjose@gmail.com>
2022-11-23 16:26:27 -0500

    profile own pins and saveds

--- shareme_frontend/src/components/UserProfile.jsx
+++ shareme_frontend/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { AiOutlineLogout } from 'react-icons/ai';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, redirect } from 'react-router-dom';
 import { googleLogout } from '@react-oauth/google';
 
 import { userCreatedPinsQuery, userQuery, userSavedPinsQuery } from '../utils/data'
@@ -11,6 +11,10 @@ import Spinner from './Spinner';
 const UserProfile = () => {
 
   const [ user, setUser ] = useState(null);
+  const [ text, setText ] = useState('created');
+  const [ activeBtn, setActiveBtn ] = useState('created');
+
+  const [ pins, setPins ] = useState([]);
 
   const navigate = useNavigate();
   const { userId } = useParams();
@@ -26,11 +30,41 @@ const UserProfile = () => {
       })
   }, [userId])
 
+  useEffect(()=>{
+    if(text == 'created'){
+      const CreatedPinsQuery = userCreatedPinsQuery(userId);
+  
+      client.fetch(CreatedPinsQuery)
+        .then((data) => {
+          setPins(data);
+          console.log("created", {data});
+        })
+    }else{
+      const savedPinsQuery = userSavedPinsQuery(userId);
+  
+      client.fetch(savedPinsQuery)
+        .then((data) => {
+          setPins(data);
+          console.log("saved",{data});
+        })
+    }
+  }, [text, userId])
+
   const bgPrifile = {
-    backgroundImage: 'url("https://images.unsplash.com/photo-1661074670187-3237f445f10d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")',
+    backgroundImage: 'url("https://source.unsplash.com/1600x900/?nature,photography,technology")',
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat'
   }
+
+  const logout = () => {
+    localStorage.clear();
+    
+    navigate('/login');
+  }
+
+  const activeBtnStyles = 'bg-red-500 text-white font-bold p-2 rounded-full w-20 outline-none';
+  const notActiveBtnStyles = 'bg-primary mr-4 text-black font-bold p-2 rounded-full w-20 outline-none';
+
 
   if(!user){
     return ( <Spinner message={'Loading User'} /> )
@@ -43,8 +77,48 @@ const UserProfile = () => {
             <img src={user?.image} alt="User Image" className='rounded-full'/>
           </div>
           <div>
-            <p>{ user?.userName }</p>
+            <p className='font-bold text-xl text-white'>{ user?.userName }</p>
           </div>
+          <div className='absolute top-1 z-1 right-10 p-2'>
+            { userId === user._id && (
+              <button onClick={logout}
+                className='bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded'>
+                Logout
+              </button>
+            )}
+          </div>
+      </div>
+      <div className='mt-2'>
+        <div className='text-center mb-7'>
+          <button
+            type='button'
+            onClick={(e) => {
+              setText(e.target.textContent.toLowerCase());
+              setActiveBtn('created');
+            }}
+            className={`${activeBtn === 'created' ? activeBtnStyles : notActiveBtnStyles}`}
+          >
+            Created
+          </button>
+          <button
+            type='button'
+            onClick={(e) => {
+              setText(e.target.textContent.toLowerCase());
+              setActiveBtn('saved');
+            }}
+            className={`${activeBtn === 'saved' ? activeBtnStyles : notActiveBtnStyles}`}
+          >
+            Saved
+          </button>
+        </div>
+        { pins?.length ? (
+          <div className='px-2'>
+            <MasonryLayout pins={pins}/>
+          </div>
+        ):(
+          <div className='px-2 w-full text-center'>There is no pins yet!, Create One...</div>
+        ) }
+        
       </div>
     </div>
   )
